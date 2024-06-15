e66a24cdacc711111831994e387b9fa9a87b4693
frankieandone <22060147+frankieandone@users.noreply.github.com>
2024-06-15 18:31:26 +0100

    New: tealdeer

--- .aliases.zsh
+++ .aliases.zsh
@@ -9,8 +9,11 @@ if command fleet -V &>/dev/null; then
     alias dotconfig='fleet "$DOTFILES"'
 elif command code -v &>/dev/null; then
     alias dotconfig='code -n "$DOTFILES"'
+elif command nvim -v &>/dev/null; then
+    alias dotconfig='nvim "$DOTFILES"'
 fi
 
-if [ -f /Applications/IntelliJ\ IDEA\ CE.app/Contents/MacOS/idea ]; then
-    alias idea='/Applications/IntelliJ\ IDEA\ CE.app/Contents/MacOS/idea'
+if command tldr -v &>/dev/null; then
+  alias man='tldr'
+  alias def='tldr'
 fi
