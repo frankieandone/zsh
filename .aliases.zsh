83a27399ede604c199f627d8efdb6e695b91086f
frankieandone <22060147+frankieandone@users.noreply.github.com>
2024-06-16 13:40:19 +0100

    Refactor: aliases

--- .aliases.zsh
+++ .aliases.zsh
@@ -1,19 +1,28 @@
 #!/usr/bin/env zsh
 
-if command -v nvim &>/dev/null; then
-    EDITOR=nvim
-    alias vim=nvim
-fi
+setup_editor() {
+    export EDITOR='vi'
 
-if command fleet -V &>/dev/null; then
-    alias dotconfig='fleet "$DOTFILES"'
-elif command code -v &>/dev/null; then
-    alias dotconfig='code -n "$DOTFILES"'
-elif command nvim -v &>/dev/null; then
-    alias dotconfig='nvim "$DOTFILES"'
-fi
+    if command fleet -V &>/dev/null; then
+        EDITOR='fleet'
+    elif command code -v &>/dev/null; then
+        EDITOR='code -n'
+    elif command nvim -v &>/dev/null; then
+        alias vim='nvim'
+        EDITOR='nvim'
+    fi
+}
+
+setup_conf_aliases() {
+    alias dotconfig='cd ${DOTFILES} && ${EDITOR} .'
+    alias zshconfig='cd ${DOTFILES}/zsh && ${EDITOR} .'
+    alias omzconfig='cd ${HOME}/.oh-my-zsh && ${EDITOR} .'
+}
 
 if command tldr -v &>/dev/null; then
   alias man='tldr'
   alias def='tldr'
 fi
+
+setup_editor && setup_conf_aliases
+
